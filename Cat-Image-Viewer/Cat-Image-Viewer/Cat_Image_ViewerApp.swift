f6ba0209ce742505e01c82c2ae958812a40febbd
James Lea <james.q.lea@gmail.com>
2023-05-22 13:51:33 -0600

    first commit on cat image viewer

---
+++ Cat-Image-Viewer/Cat-Image-Viewer/Cat_Image_ViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cat_Image_ViewerApp.swift
+//  Cat-Image-Viewer
+//
+//  Created by James Lea on 5/22/23.
+//
+
+import SwiftUI
+
+@main
+struct Cat_Image_ViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
