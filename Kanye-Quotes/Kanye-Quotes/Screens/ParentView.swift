403d96a370836b72bafc60ccdd505feef666c590
James Lea <james.q.lea@gmail.com>
2023-06-22 15:28:54 -0600

    more testing

--- Kanye-Quotes/Kanye-Quotes/Screens/ParentView.swift
+++ Kanye-Quotes/Kanye-Quotes/Screens/ParentView.swift
@@ -1,66 +1,66 @@
-import SwiftUI
-
-class ParentViewModel: ObservableObject {
-    @Published var nestedObject: NestedObject
-
-    init() {
-        self.nestedObject = NestedObject()
-    }
-}
-
-class NestedObject {
-    var value: Int = 0
-    var anotherValue: String = ""
-}
-
-struct ParentView: View {
-    @StateObject private var parentViewModel = ParentViewModel()
-
-    var body: some View {
-        VStack {
-            Text("Nested Object Value: \(parentViewModel.nestedObject.value)")
-            Text("Nested Object Another Value: \(parentViewModel.nestedObject.anotherValue)")
-
-            ChildView(viewModel: ChildViewModel(value: $parentViewModel.nestedObject.value, anotherValue: $parentViewModel.nestedObject.anotherValue))
-                .padding()
-        }
-    }
-}
-
-class ChildViewModel: ObservableObject {
-    @Binding var value: Int
-    @Binding var anotherValue: String
-
-    init(value: Binding<Int>, anotherValue: Binding<String>) {
-        self._value = value
-        self._anotherValue = anotherValue
-    }
-}
-
-struct ChildView: View {
-    @ObservedObject var viewModel: ChildViewModel
-
-    var body: some View {
-        VStack {
-            Text("Nested Object Value: \(viewModel.value)")
-            Text("Nested Object Another Value: \(viewModel.anotherValue)")
-
-            Button("Increment Value") {
-                viewModel.value += 1
-            }
-
-            Button("Change Another Value") {
-                viewModel.anotherValue = "New Value"
-            }
-        }
-    }
-}
-
-struct ParentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ParentView()
-    }
-}
+//import SwiftUI
+//
+//class ParentViewModel: ObservableObject {
+//    @Published var nestedObject: NestedObject
+//
+//    init() {
+//        self.nestedObject = NestedObject()
+//    }
+//}
+//
+//class NestedObject {
+//    var value: Int = 0
+//    var anotherValue: String = ""
+//}
+//
+//struct ParentView: View {
+//    @StateObject private var parentViewModel = ParentViewModel()
+//
+//    var body: some View {
+//        VStack {
+//            Text("Nested Object Value: \(parentViewModel.nestedObject.value)")
+//            Text("Nested Object Another Value: \(parentViewModel.nestedObject.anotherValue)")
+//
+//            ChildView(viewModel: ChildViewModel(value: $parentViewModel.nestedObject.value, anotherValue: $parentViewModel.nestedObject.anotherValue))
+//                .padding()
+//        }
+//    }
+//}
+//
+//class ChildViewModel: ObservableObject {
+//    @Binding var value: Int
+//    @Binding var anotherValue: String
+//
+//    init(value: Binding<Int>, anotherValue: Binding<String>) {
+//        self._value = value
+//        self._anotherValue = anotherValue
+//    }
+//}
+//
+//struct ChildView: View {
+//    @ObservedObject var viewModel: ChildViewModel
+//
+//    var body: some View {
+//        VStack {
+//            Text("Nested Object Value: \(viewModel.value)")
+//            Text("Nested Object Another Value: \(viewModel.anotherValue)")
+//
+//            Button("Increment Value") {
+//                viewModel.value += 1
+//            }
+//
+//            Button("Change Another Value") {
+//                viewModel.anotherValue = "New Value"
+//            }
+//        }
+//    }
+//}
+//
+//struct ParentView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        ParentView()
+//    }
+//}
 //
 //
 //import SwiftUI
@@ -119,3 +119,63 @@ struct ParentView_Previews: PreviewProvider {
 //        ParentView()
 //    }
 //}
+
+import SwiftUI
+
+struct Itemo: Identifiable, Equatable {
+    let id = UUID()
+    var value: Int
+}
+
+class ParentViewModel: ObservableObject {
+    @Published var items: NSSet
+    
+    init() {
+        let item1 = Itemo(value: 1)
+        let item2 = Itemo(value: 2)
+        let item3 = Itemo(value: 3)
+        
+        self.items = NSSet(array: [item1, item2, item3])
+    }
+}
+
+struct ParentView: View {
+    @StateObject private var parentViewModel = ParentViewModel()
+    
+    var body: some View {
+        VStack {
+            ChildView(items: $parentViewModel.items)
+        }
+    }
+}
+
+struct ChildView: View {
+    @Binding var items: NSSet
+    
+    var body: some View {
+        List {
+            ForEach(Array(items) as! [Itemo]) { item in
+                VStack {
+                    Text("Value: \(item.value)")
+                    Button("Increment Value") {
+                        updateItemValue(item)
+                    }
+                }
+            }
+        }
+    }
+    
+    private func updateItemValue(_ item: Itemo) {
+        let mutableItems = items.mutableCopy() as! NSMutableSet
+        if var mutableItem = mutableItems.allObjects.first(where: { ($0 as? Itemo)?.id == item.id }) as? Itemo {
+            mutableItem.value += 1
+        }
+        items = mutableItems.copy() as! NSSet
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ParentView()
+    }
+}
