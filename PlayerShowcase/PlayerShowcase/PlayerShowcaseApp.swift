a22fd10c33570fb2ef78d79859015da0eb4eddbf
James Lea <james.q.lea@gmail.com>
2023-07-11 12:14:21 -0600

    first commit

---
+++ PlayerShowcase/PlayerShowcase/PlayerShowcaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlayerShowcaseApp.swift
+//  PlayerShowcase
+//
+//  Created by James Lea on 7/11/23.
+//
+
+import SwiftUI
+
+@main
+struct PlayerShowcaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
