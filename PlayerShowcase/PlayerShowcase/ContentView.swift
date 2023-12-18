f0719562d52f2b3807e8681e3aca35dcbf4f3f5b
James Lea <james.q.lea@gmail.com>
2023-12-17 19:56:43 -0800

    UITabBar tint color

--- PlayerShowcase/PlayerShowcase/ContentView.swift
+++ PlayerShowcase/PlayerShowcase/ContentView.swift
@@ -8,10 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var count: Int = 0
     var body: some View {
         VStack {
-            
+            VStack {
+                Text("\(count)")
+                HStack {
+                    Button {
+                        plusOne()
+                    } label: {
+                        Text("+")
+                    }
+                    Button {
+                        minusOne()
+                    } label: {
+                        Text("-")
+                    }
+                }
+            }
         }
+    }
+    
+    func plusOne() {
+        count += 1
+    }
+    
+    func minusOne() {
+        count -= 1
+    }
+    
+    func goyslop() {
+        print("McDonald")
     }
 }
 
